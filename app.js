da6f8ffed887cfda17c980892013eff3d5e40e9a
frameur <francoisramos6121@gmail.com>
2020-05-14 15:42:59 +0200

    ajout webp

--- app.js
+++ app.js
@@ -32,7 +32,8 @@ const upload = multer({
       file.mimetype === "image/png" ||
       file.mimetype === "image/jpg" ||
       file.mimetype === "image/jpeg" ||
-      file.mimetype === "image/gif"
+      file.mimetype === "image/gif"  ||
+      file.mimetype === "image/webp"
     ) {
       cb(null, true);
     } else
