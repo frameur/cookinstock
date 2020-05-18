7b0d65b0c080eb7a56aedb2a8d40da6b7d52c44e
frameur <francoisramos6121@gmail.com>
2020-05-18 17:10:41 +0200

    remove body parser

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 const express = require('express')
 const mongoose = require('mongoose')
 const nunjucks = require('nunjucks')
-// const bodyparser = require('body-parser')
 const multer = require('multer')
 const path = require('path')
 
